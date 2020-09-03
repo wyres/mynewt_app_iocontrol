a406c486fe16e3834f26725802a72506bffa57cb
Brian Wyld <brian.wyld@wyres.fr>
2020-09-03 22:27:50 +0200

    update for latest hex_maker batch file

--- hex_maker.bat
+++ hex_maker.bat
@@ -2,5 +2,7 @@ set BASE=%CD%
 
 newt build %1
 newt create-image -2 %1 0.0.0.1
-newt mfg create %1 0.0.0.1
-arm-none-eabi-objcopy -I ihex -O ihex --change-addresses=0x08000000 %BASE%\bin\mfgs\%1\mfgimg.hex %BASE%\hexTargets\%1.hex
+newt -v mfg create %1 0.0.0.1
+:: check if source is .bin or .hex and ajust the objcopy
+:: TODO
+arm-none-eabi-objcopy -I binary -O ihex --change-addresses=0x08000000 %BASE%\bin\mfgs\%1\mfgimg.bin %BASE%\hexTargets\%1.hex

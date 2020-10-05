12e57bdce29b0ee469a18e56afa4cb72e3af8df5
Brian Wyld <brian.wyld@wyres.fr>
2020-10-05 12:39:28 +0200

    version with explicit boot target and exit if any command fails

--- hex_maker.bat
+++ hex_maker.bat
@@ -1,18 +1,30 @@
+echo off
 set BASE=%CD%
 
+set TARGET=%1
+if "%TARGET%"=="boot" (
 :: must have done a newt build on the bootloaders already
-newt build wproto_bootloader
+    echo "Building bootloader"
+    newt build wbasev2_bootloader || EXIT /B 1
 :: newt build wbasev2_bootloader not yet done
+    exit /B 0
+)
+if "%TARGET%"=="version" (
+    newt version || EXIT /B 1
+    exit /B 0
+)
 
 :: now our target
-newt build %1
-newt create-image -2 %1 0.0.0.1
-newt mfg create %1 0.0.0.1
-arm-none-eabi-objcopy -I binary -O ihex --change-addresses=0x08000000 %BASE%\bin\mfgs\%1\mfgimg.bin %BASE%\built\%1.hex
+echo "Building target %TARGET% : starting"
+newt build %TARGET% || EXIT /B 1
+newt create-image -2 %TARGET% 0.0.0.1 || EXIT /B 1
+newt mfg create %TARGET% 0.0.0.1 || EXIT /B 1
+arm-none-eabi-objcopy -I binary -O ihex --change-addresses=0x08000000 %BASE%\bin\mfgs\%TARGET%\mfgimg.bin %BASE%\built\%TARGET%.hex || EXIT /B 1
 
 :: copy the results to the built dir to be able to find and identify them easier
-cp %BASE%/bin/targets/%1/app/apps/appcorerun/appcorerun.elf %BASE%/built/%1.elf
+cp %BASE%/bin/targets/%TARGET%/app/apps/appcorerun/appcorerun.elf %BASE%/built/%TARGET%.elf || EXIT /B 1
 
 :: ensure the current.elf is the elf we want to debug (so VC knows) and current.hex for the flash script
-cp %BASE%/built/%1.elf %BASE%/built/current.elf
-cp %BASE%/built/%1.hex %BASE%/built/current.hex
+cp %BASE%/built/%TARGET%.elf %BASE%/built/current.elf || EXIT /B 1
+cp %BASE%/built/%TARGET%.hex %BASE%/built/current.hex || EXIT /B 1
+echo "Finished target %TARGET% : result is in %BASE%/built"
